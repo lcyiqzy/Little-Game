80589bead4ae79495659771ce50dac3e2ac2243e
osbel <osbel.zhou.gmail.com>
2016-01-10 00:33:58 +0800

    Alittle letter game

---
+++ app/src/main/java/com/example/administrator/littlegame/MainActivity.java
@@ -0,0 +1,165 @@
+package com.example.administrator.littlegame;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    TextView textView;
+    Button b1;
+    Button b2;
+    Button b3;
+    Button b4;
+    Button b5;
+    Button b6;
+    Button b7;
+    Button b8;
+    Button b9;
+    Button b10;
+    Button b11;
+    Button b12;
+    Button b13;
+    Button b14;
+    Button b15;
+    Button b16;
+    Button b17;
+    Button b18;
+    Button b19;
+    Button b20;
+    Button b21;
+    Button b22;
+    Button b23;
+    Button b24;
+    Button b25;
+    Button b26;
+    Button b27;
+    Button b28;
+    Button b29;
+    Button b30;
+    Button b31;
+    Button b32;
+    Button b33;
+    Button b34;
+    Button b35;
+    Button b36;
+    Button b37;
+    Button b38;
+    Button b39;
+    Button b40;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b1 = (Button) findViewById(R.id.b1);
+        b2 = (Button) findViewById(R.id.b2);
+        b3 = (Button) findViewById(R.id.b3);
+        b4 = (Button) findViewById(R.id.b4);
+        b5 = (Button) findViewById(R.id.b5);
+        b6 = (Button) findViewById(R.id.b6);
+        b7 = (Button) findViewById(R.id.b7);
+        b8 = (Button) findViewById(R.id.b8);
+        b9 = (Button) findViewById(R.id.b9);
+        b10 = (Button) findViewById(R.id.b10);
+        b11 = (Button) findViewById(R.id.b11);
+        b12 = (Button) findViewById(R.id.b12);
+        b13 = (Button) findViewById(R.id.b13);
+        b14 = (Button) findViewById(R.id.b14);
+        b15 = (Button) findViewById(R.id.b15);
+        b16 = (Button) findViewById(R.id.b16);
+        b17 = (Button) findViewById(R.id.b17);
+        b18 = (Button) findViewById(R.id.b18);
+        b19 = (Button) findViewById(R.id.b19);
+        b20 = (Button) findViewById(R.id.b20);
+        b21 = (Button) findViewById(R.id.b21);
+        b22 = (Button) findViewById(R.id.b22);
+        b23 = (Button) findViewById(R.id.b23);
+        b24 = (Button) findViewById(R.id.b24);
+        b25 = (Button) findViewById(R.id.b25);
+        b26 = (Button) findViewById(R.id.b26);
+        b27 = (Button) findViewById(R.id.b27);
+        b28 = (Button) findViewById(R.id.b28);
+        b29 = (Button) findViewById(R.id.b29);
+        b30 = (Button) findViewById(R.id.b30);
+        b31 = (Button) findViewById(R.id.b31);
+        b32 = (Button) findViewById(R.id.b32);
+        b33 = (Button) findViewById(R.id.b33);
+        b34 = (Button) findViewById(R.id.b34);
+        b35 = (Button) findViewById(R.id.b35);
+        b36 = (Button) findViewById(R.id.b36);
+        b37 = (Button) findViewById(R.id.b37);
+        b38 = (Button) findViewById(R.id.b38);
+        b39 = (Button) findViewById(R.id.b39);
+        b40 = (Button) findViewById(R.id.b40);
+        textView = (TextView) findViewById(R.id.textView);
+
+
+        b1.setOnClickListener(this);
+        b1.setOnClickListener(this);
+        b2.setOnClickListener(this);
+        b3.setOnClickListener(this);
+        b4.setOnClickListener(this);
+        b5.setOnClickListener(this);
+        b6.setOnClickListener(this);
+        b7.setOnClickListener(this);
+        b8.setOnClickListener(this);
+        b9.setOnClickListener(this);
+        b10.setOnClickListener(this);
+        b11.setOnClickListener(this);
+        b12.setOnClickListener(this);
+        b13.setOnClickListener(this);
+        b14.setOnClickListener(this);
+        b15.setOnClickListener(this);
+        b16.setOnClickListener(this);
+        b17.setOnClickListener(this);
+        b18.setOnClickListener(this);
+        b19.setOnClickListener(this);
+        b20.setOnClickListener(this);
+        b21.setOnClickListener(this);
+        b22.setOnClickListener(this);
+        b23.setOnClickListener(this);
+        b24.setOnClickListener(this);
+        b25.setOnClickListener(this);
+        b26.setOnClickListener(this);
+        b27.setOnClickListener(this);
+        b28.setOnClickListener(this);
+        b29.setOnClickListener(this);
+        b30.setOnClickListener(this);
+        b31.setOnClickListener(this);
+        b32.setOnClickListener(this);
+        b33.setOnClickListener(this);
+        b34.setOnClickListener(this);
+        b35.setOnClickListener(this);
+        b36.setOnClickListener(this);
+        b37.setOnClickListener(this);
+        b38.setOnClickListener(this);
+        b39.setOnClickListener(this);
+        b40.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Button b= (Button) v;
+
+        if( b.getText().equals("A")){
+
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
